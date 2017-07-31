4b812831ceba45c297206b37a9b225453b898794
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-07-31 21:50:21 +0800

    wip

--- bingobingo_playground/hangod_predict.py
+++ bingobingo_playground/hangod_predict.py
@@ -12,7 +12,7 @@ def get_trained_clf(features, labels):
     return clf
 
 def prepare_hangod_features_labels(bingobingo_dataframe):
-    features = bingobingo_dataframe.drop(['number54'], axis=1).drop(0)
+    features = bingobingo_dataframe.drop(0)
     labels = bingobingo_dataframe['number54'][:-1]
-    newest_feature = bingobingo_dataframe.drop(['number54'], axis=1)[:1]
+    newest_feature = bingobingo_dataframe[:1]
     return features, labels, newest_feature
