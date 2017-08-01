406bfac3bc9f472e5a422f3745b626ed4e042b6a
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-08-01 11:29:46 +0800

    WIP

--- setup.py
+++ setup.py
@@ -5,5 +5,15 @@ setup(
     packages=find_packages(),
     author='gychen',
     author_email='gy.chen@gms.nutc.edu.tw',
-    install_requires=['pandas', 'scikit-learn', 'sqlalchemy', 'beautifulsoup4', 'gunicorn', 'flask', 'matplotlib', 'numpy', 'scipy']
+    install_requires=[
+        'pandas',
+        'scikit-learn',
+        'sqlalchemy',
+        'beautifulsoup4',
+        'gunicorn',
+        'flask',
+        'matplotlib',
+        'numpy',
+        'scipy',
+        'requests']
 )
